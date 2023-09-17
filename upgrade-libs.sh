1fe7a6eca46133adf43adc1bccf884afc7f92bdf
Haci Murat Yaman <haci@muratyaman.co.uk>
2023-09-17 11:42:36 +0100

    upgrade node modules

---
+++ upgrade-libs.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+npm i -D @istanbuljs/nyc-config-typescript
+npm i -D @types/bcrypt
+npm i -D @types/chai
+npm i -D @types/jsonwebtoken
+npm i -D @types/mocha
+npm i -D @types/node
+npm i -D @typescript-eslint/eslint-plugin
+npm i -D @typescript-eslint/parser
+npm i -D chai
+npm i -D eslint
+npm i -D mocha
+npm i -D nyc
+npm i -D rimraf
+npm i -D ts-node
+npm i -D typescript
+
+
+npm i ajv
+npm i axios
+npm i bcrypt
+npm i date-fns
+npm i jsonwebtoken
+npm i redis
